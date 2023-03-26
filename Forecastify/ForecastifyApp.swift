eb758c4c8e63628a4c48dc9e5be7c0616d63702d
Jinwoo Kim <kidjinwoo@me.com>
2023-03-27 03:00:38 +0900

    no message

---
+++ Forecastify/ForecastifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastifyApp.swift
+//  Forecastify
+//
+//  Created by Jinwoo Kim on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Forecastify_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
